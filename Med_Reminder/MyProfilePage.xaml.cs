b3a5063d49d30164b0e9337cc0c50dbf6af2bf3b
KornelBujak <kb301655@student.polsl.pl>
2024-04-28 12:13:20 +0200

    no message

--- Med_Reminder/MyProfilePage.xaml.cs
+++ Med_Reminder/MyProfilePage.xaml.cs
@@ -18,27 +18,26 @@ public partial class MyProfilePage : ContentPage
     {
         base.OnAppearing();
 
-        // SprawdŸ, czy u¿ytkownik jest zalogowany
+        
         if (App.IsUserLoggedIn)
         {
             await LoadUserData();
         }
         else
         {
-            // Przekieruj u¿ytkownika do strony logowania, jeœli nie jest zalogowany
+           
             await Navigation.PushAsync(new LoginPage());
         }
     }
 
     private async Task LoadUserData()
     {
-        // Pobierz ID aktualnie zalogowanego u¿ytkownika
+        
         int userId = App.CurrentUserId;
 
-        // Pobierz dane u¿ytkownika z repozytorium
+       
         DaneOsobowe daneOsobowe = await _userProfileRepository.GetDaneOsoboweAsync(userId);
 
-        // Wyœwietl dane u¿ytkownika na stronie
         if (daneOsobowe != null)
         {
             ImieLabel.Text = "Imiê: " + daneOsobowe.Imie;
@@ -52,16 +51,16 @@ public partial class MyProfilePage : ContentPage
 
     private async void SaveChangesButton_Clicked(object sender, EventArgs e)
 {
-    // Pobierz zmienione dane z pól formularza
+   
     string newName = NewNameEntry.Text;
     string newEmail = NewEmailEntry.Text;
     string newPassword = NewPasswordEntry.Text;
     double newWeight = Convert.ToDouble(NewWeightEntry.Text);
 
-    // Pobierz obecne dane u¿ytkownika
+    
     DaneOsobowe currentUser = await _userProfileRepository.GetDaneOsoboweAsync(App.CurrentUserId);
 
-    // Zaktualizuj dane u¿ytkownika
+    
     if (!string.IsNullOrEmpty(newName))
         currentUser.Nazwisko = newName;
 
@@ -73,20 +72,19 @@ public partial class MyProfilePage : ContentPage
 
     currentUser.Waga = newWeight;
 
-    // Zapisz zmiany do bazy danych
+    
     _userProfileRepository.UpdateDaneOsobowe(currentUser);
     await _userProfileRepository.SaveChanges();
 
-    // Wyczyœæ zawartoœæ pól formularza
+   
     NewNameEntry.Text = string.Empty;
     NewEmailEntry.Text = string.Empty;
     NewPasswordEntry.Text = string.Empty;
     NewWeightEntry.Text = string.Empty;
 
-    // Wyœwietl komunikat o sukcesie
+    
     await DisplayAlert("Zapisano zmiany", "Twoje dane zosta³y zaktualizowane.", "OK");
 
-    // Ponownie wczytaj dane u¿ytkownika, aby odœwie¿yæ widok
     await LoadUserData();
 }
 
